0a8f32807d7fe7067702bfbb27591e5c39a8aefc
RafaAudibert <rafaeelaudibert@gmail.com>
2021-11-29 14:11:09 +0100

    Run black formatter

--- BP_Decoder.py
+++ BP_Decoder.py
@@ -4,13 +4,14 @@ from tqdm import tqdm
 # We are using TF V1 behavior by default here
 # import tensorflow as tf
 import tensorflow.compat.v1 as tf
+
 tf.disable_v2_behavior()
 
 
 class GetMatrixForBPNet:
-    '''
+    """
     Calculates the matrices used to perform BP process with matrix operation
-    '''
+    """
 
     def __init__(self, test_H, loc_nzero_row):
         print("Construct the Matrics H class!\n", flush=True)
@@ -21,29 +22,22 @@ class GetMatrixForBPNet:
         self.loc_nzero_row = loc_nzero_row
         self.num_all_edges = np.size(self.loc_nzero_row[1, :])
 
-        self.loc_nzero1 = self.loc_nzero_row[1,
-                                             :] * self.n + self.loc_nzero_row[0, :]
+        self.loc_nzero1 = self.loc_nzero_row[1, :] * self.n + self.loc_nzero_row[0, :]
         self.loc_nzero2 = np.sort(self.loc_nzero1)
-        self.loc_nzero_line = np.append([np.mod(self.loc_nzero2, self.n)], [
-                                        self.loc_nzero2 // self.n], axis=0)
-        self.loc_nzero4 = self.loc_nzero_line[0,
-                                              :] * self.n + self.loc_nzero_line[1, :]
+        self.loc_nzero_line = np.append([np.mod(self.loc_nzero2, self.n)], [self.loc_nzero2 // self.n], axis=0)
+        self.loc_nzero4 = self.loc_nzero_line[0, :] * self.n + self.loc_nzero_line[1, :]
         self.loc_nzero5 = np.sort(self.loc_nzero4)
 
     def get_Matrix_VC(self):
         H_x_to_xe0 = np.zeros([self.num_all_edges, self.n], np.float32)
-        H_sum_by_V_to_C = np.zeros(
-            [self.num_all_edges, self.num_all_edges], dtype=np.float32)
-        H_xe_last_to_y = np.zeros(
-            [self.n, self.num_all_edges], dtype=np.float32)
+        H_sum_by_V_to_C = np.zeros([self.num_all_edges, self.num_all_edges], dtype=np.float32)
+        H_xe_last_to_y = np.zeros([self.n, self.num_all_edges], dtype=np.float32)
         Map_row_to_line = np.zeros([self.num_all_edges, 1])
 
         for i in tqdm(range(0, self.num_all_edges), desc="Row to line"):
-            Map_row_to_line[i] = np.where(
-                self.loc_nzero1 == self.loc_nzero2[i])
+            Map_row_to_line[i] = np.where(self.loc_nzero1 == self.loc_nzero2[i])
 
-        map_H_row_to_line = np.zeros(
-            [self.num_all_edges, self.num_all_edges], dtype=np.float32)
+        map_H_row_to_line = np.zeros([self.num_all_edges, self.num_all_edges], dtype=np.float32)
 
         for i in tqdm(range(0, self.num_all_edges), desc="H Row to line"):
             map_H_row_to_line[i, int(Map_row_to_line[i])] = 1
@@ -62,17 +56,14 @@ class GetMatrixForBPNet:
         return H_x_to_xe0, np.matmul(H_sum_by_V_to_C, map_H_row_to_line), np.matmul(H_xe_last_to_y, map_H_row_to_line)
 
     def get_Matrix_CV(self):
-        H_sum_by_C_to_V = np.zeros(
-            [self.num_all_edges, self.num_all_edges], dtype=np.float32)
+        H_sum_by_C_to_V = np.zeros([self.num_all_edges, self.num_all_edges], dtype=np.float32)
 
         Map_line_to_row = np.zeros([self.num_all_edges, 1])
 
         for i in tqdm(range(0, self.num_all_edges), desc="Line to row"):
-            Map_line_to_row[i] = np.where(
-                self.loc_nzero4 == self.loc_nzero5[i])
+            Map_line_to_row[i] = np.where(self.loc_nzero4 == self.loc_nzero5[i])
 
-        map_H_line_to_row = np.zeros(
-            [self.num_all_edges, self.num_all_edges], dtype=np.float32)
+        map_H_line_to_row = np.zeros([self.num_all_edges, self.num_all_edges], dtype=np.float32)
 
         for i in tqdm(range(0, np.size(self.loc_nzero1)), desc="H line to row"):
             map_H_line_to_row[i, int(Map_line_to_row[i])] = 1
@@ -99,13 +90,17 @@ class BP_NetDecoder:
         self.H_sumC_to_V = gm1.get_Matrix_CV()
         self.H_x_to_xe0, self.H_sumV_to_C, self.H_xe_v_sumc_to_y = gm1.get_Matrix_VC()
         self.batch_size = batch_size
-        self.llr_placeholder = tf.placeholder(
-            tf.float32, [batch_size, self.v_node_num])
-        self.llr_into_bp_net, self.xe_0, self.xe_v2c_pre_iter_assign, self.start_next_iteration, self.dec_out = self.build_network()
+        self.llr_placeholder = tf.placeholder(tf.float32, [batch_size, self.v_node_num])
+        (
+            self.llr_into_bp_net,
+            self.xe_0,
+            self.xe_v2c_pre_iter_assign,
+            self.start_next_iteration,
+            self.dec_out,
+        ) = self.build_network()
 
         # transpose the llr matrix to adapt to the matrix operation in BP net decoder.
-        self.llr_assign = self.llr_into_bp_net.assign(
-            tf.transpose(self.llr_placeholder))
+        self.llr_assign = self.llr_into_bp_net.assign(tf.transpose(self.llr_placeholder))
 
         init = tf.global_variables_initializer()
         self.sess = tf.Session()  # open a session
@@ -125,10 +120,8 @@ class BP_NetDecoder:
         xe_tanh = tf.tanh(tf.to_double(tf.truediv(xe_v2c_pre_iter, [2.0])))
         xe_tanh = tf.to_float(xe_tanh)
         xe_tanh_temp = tf.sign(xe_tanh)
-        xe_sum_log_img = tf.matmul(H_sumC_to_V, tf.multiply(
-            tf.truediv((1 - xe_tanh_temp), [2.0]), [3.1415926]))
-        xe_sum_log_real = tf.matmul(
-            H_sumC_to_V, tf.log(1e-8 + tf.abs(xe_tanh)))
+        xe_sum_log_img = tf.matmul(H_sumC_to_V, tf.multiply(tf.truediv((1 - xe_tanh_temp), [2.0]), [3.1415926]))
+        xe_sum_log_real = tf.matmul(H_sumC_to_V, tf.log(1e-8 + tf.abs(xe_tanh)))
         xe_sum_log_complex = tf.complex(xe_sum_log_real, xe_sum_log_img)
         xe_product = tf.real(tf.exp(xe_sum_log_complex))
         xe_product_temp = tf.multiply(tf.sign(xe_product), -2e-7)
@@ -138,33 +131,28 @@ class BP_NetDecoder:
         return xe_v_sumc, xe_c_sumv
 
     def build_network(self):
-        '''
+        """
         Builds the network for one BP iteration
-        '''
+        """
 
         # BP initialization
-        llr_into_bp_net = tf.Variable(
-            np.ones([self.v_node_num, self.batch_size], dtype=np.float32))
+        llr_into_bp_net = tf.Variable(np.ones([self.v_node_num, self.batch_size], dtype=np.float32))
         xe_0 = tf.matmul(self.H_x_to_xe0, llr_into_bp_net)
         # the v->c messages of the previous iteration
-        xe_v2c_pre_iter = tf.Variable(
-            np.ones([self.num_all_edges, self.batch_size], dtype=np.float32))
+        xe_v2c_pre_iter = tf.Variable(np.ones([self.num_all_edges, self.batch_size], dtype=np.float32))
         xe_v2c_pre_iter_assign = xe_v2c_pre_iter.assign(xe_0)
 
         # one iteration
         H_sumC_to_V = tf.constant(self.H_sumC_to_V, dtype=tf.float32)
         H_sumV_to_C = tf.constant(self.H_sumV_to_C, dtype=tf.float32)
-        xe_v_sumc, xe_c_sumv = self.one_bp_iteration(
-            xe_v2c_pre_iter, H_sumC_to_V, H_sumV_to_C, xe_0)
+        xe_v_sumc, xe_c_sumv = self.one_bp_iteration(xe_v2c_pre_iter, H_sumC_to_V, H_sumV_to_C, xe_0)
 
         # start the next iteration
         start_next_iteration = xe_v2c_pre_iter.assign(xe_c_sumv)
 
         # get the final marginal probability and decoded results
-        bp_out_llr = tf.add(llr_into_bp_net, tf.matmul(
-            self.H_xe_v_sumc_to_y, xe_v_sumc))
-        dec_out = tf.transpose(tf.floordiv(
-            1-tf.to_int32(tf.sign(bp_out_llr)), 2))
+        bp_out_llr = tf.add(llr_into_bp_net, tf.matmul(self.H_xe_v_sumc_to_y, xe_v_sumc))
+        dec_out = tf.transpose(tf.floordiv(1 - tf.to_int32(tf.sign(bp_out_llr)), 2))
 
         return llr_into_bp_net, xe_0, xe_v2c_pre_iter_assign, start_next_iteration, dec_out
 
@@ -172,14 +160,12 @@ class BP_NetDecoder:
         real_batch_size, num_v_node = np.shape(llr_in)
         if real_batch_size != self.batch_size:  # padding zeros
             # re-create an array and will not influence the value in
-            llr_in = np.append(llr_in, np.zeros(
-                [self.batch_size-real_batch_size, num_v_node], dtype=np.float32), 0)
+            llr_in = np.append(llr_in, np.zeros([self.batch_size - real_batch_size, num_v_node], dtype=np.float32), 0)
             # original llr array.
-        self.sess.run(self.llr_assign, feed_dict={
-                      self.llr_placeholder: llr_in})
+        self.sess.run(self.llr_assign, feed_dict={self.llr_placeholder: llr_in})
         self.sess.run(self.xe_v2c_pre_iter_assign)
 
-        for _ in range(0, bp_iter_num-1):
+        for _ in range(0, bp_iter_num - 1):
             self.sess.run(self.start_next_iteration)
 
         y_dec = self.sess.run(self.dec_out)
--- DataIO.py
+++ DataIO.py
@@ -3,6 +3,7 @@ import numpy as np
 # We are using TF V1 behavior by default here
 # import tensorflow as tf
 import tensorflow.compat.v1 as tf
+
 tf.disable_v2_behavior()
 
 # this file defines classes for data io
@@ -10,7 +11,6 @@ tf.disable_v2_behavior()
 
 class TrainingDataIO:
     def __init__(self, feature_filename, label_filename, total_trainig_samples, feature_length, label_length):
-        print("Construct the data IO class for training!", flush=True)
         self.fin_label = open(label_filename, "rb")
         self.fin_feature = open(feature_filename, "rb")
         self.total_trainig_samples = total_trainig_samples
@@ -18,15 +18,14 @@ class TrainingDataIO:
         self.label_length = label_length
 
     def __del__(self):
-        print("Delete the data IO class!", flush=True)
         self.fin_feature.close()
         self.fin_label.close()
 
     def load_next_mini_batch(self, mini_batch_size, factor_of_start_pos=1):
-        '''
+        """
         loadS the next batch where the datas in the batch are from a continuous memory block
         the start position for reading data must be a multiple of factor_of_start_pos
-        '''
+        """
         remain_samples = mini_batch_size
 
         # output a single value which is less than total_trainig_samples
@@ -34,18 +33,15 @@ class TrainingDataIO:
         features = np.zeros((0))
         labels = np.zeros((0))
         if mini_batch_size > self.total_trainig_samples:
-            print(
-                "Mini batch size should not be larger than total sample size!", flush=True)
-        self.fin_feature.seek((self.feature_length * 4) * (sample_id //
-                              factor_of_start_pos*factor_of_start_pos), 0)  # float32 = 4 bytes = 32 bits
-        self.fin_label.seek((self.label_length * 4) *
-                            (sample_id//factor_of_start_pos*factor_of_start_pos), 0)
+            print("Mini batch size should not be larger than total sample size!", flush=True)
+        self.fin_feature.seek(
+            (self.feature_length * 4) * (sample_id // factor_of_start_pos * factor_of_start_pos), 0
+        )  # float32 = 4 bytes = 32 bits
+        self.fin_label.seek((self.label_length * 4) * (sample_id // factor_of_start_pos * factor_of_start_pos), 0)
 
         while True:
-            new_feature = np.fromfile(
-                self.fin_feature, np.float32, self.feature_length * remain_samples)
-            new_label = np.fromfile(
-                self.fin_label, np.float32, self.label_length * remain_samples)
+            new_feature = np.fromfile(self.fin_feature, np.float32, self.feature_length * remain_samples)
+            new_label = np.fromfile(self.fin_label, np.float32, self.label_length * remain_samples)
             features = np.concatenate((features, new_feature))
             labels = np.concatenate((labels, new_label))
             remain_samples -= len(new_feature) // self.feature_length
@@ -77,9 +73,9 @@ class TestDataIO:
         self.fin_label.close()
 
     def seek_file_to_zero(self):
-        '''
+        """
         Reset the file pointer to the start of the file
-        '''
+        """
         self.fin_feature.seek(0, 0)
         self.fin_label.seek(0, 0)
 
@@ -87,19 +83,13 @@ class TestDataIO:
         if batch_size > self.test_sample_num:
             print("Batch size should not be larger than total sample size!", flush=True)
         if np.size(self.all_features) == 0:
-            self.all_features = np.fromfile(
-                self.fin_feature, np.float32, self.feature_length * self.test_sample_num)
-            self.all_labels = np.fromfile(
-                self.fin_label, np.float32, self.label_length * self.test_sample_num)
-            self.all_features = np.reshape(
-                self.all_features, [self.test_sample_num, self.feature_length])
-            self.all_labels = np.reshape(
-                self.all_labels, [self.test_sample_num, self.label_length])
+            self.all_features = np.fromfile(self.fin_feature, np.float32, self.feature_length * self.test_sample_num)
+            self.all_labels = np.fromfile(self.fin_label, np.float32, self.label_length * self.test_sample_num)
+            self.all_features = np.reshape(self.all_features, [self.test_sample_num, self.feature_length])
+            self.all_labels = np.reshape(self.all_labels, [self.test_sample_num, self.label_length])
 
-        features = self.all_features[self.data_position:(
-            self.data_position + batch_size), :]
-        labels = self.all_labels[self.data_position:(
-            self.data_position + batch_size), :]
+        features = self.all_features[self.data_position : (self.data_position + batch_size), :]
+        labels = self.all_labels[self.data_position : (self.data_position + batch_size), :]
         self.data_position += batch_size
         if self.data_position >= self.test_sample_num:
             self.data_position = 0
@@ -112,21 +102,19 @@ class NoiseIO:
         self.blk_len = blk_len
         self.rng_seed = rng_seed
         if read_from_file:
-            self.fin_noise = open(noise_file, 'rb')
+            self.fin_noise = open(noise_file, "rb")
         else:
             self.rng = np.random.RandomState(rng_seed)
-            fin_cov_file = open(cov_1_2_mat_file_gen_noise, 'rb')
-            cov_1_2_mat = np.fromfile(
-                fin_cov_file, np.float32, blk_len*blk_len)
+            fin_cov_file = open(cov_1_2_mat_file_gen_noise, "rb")
+            cov_1_2_mat = np.fromfile(fin_cov_file, np.float32, blk_len * blk_len)
             cov_1_2_mat = np.reshape(cov_1_2_mat, [blk_len, blk_len])
             fin_cov_file.close()
 
             # output parts of the correlation function for check
             cov_func = np.matmul(cov_1_2_mat, cov_1_2_mat)
-            print('Correlation function of channel noise: ')
+            print("Correlation function of channel noise: ")
             print(cov_func[0, 0:10])
-            self.awgn_noise = tf.placeholder(
-                dtype=tf.float32, shape=[None, blk_len])
+            self.awgn_noise = tf.placeholder(dtype=tf.float32, shape=[None, blk_len])
             self.noise_tf = tf.matmul(self.awgn_noise, cov_1_2_mat)
             self.sess = tf.Session()
 
@@ -137,9 +125,9 @@ class NoiseIO:
             self.sess.close()
 
     def reset_noise_generator(self):
-        '''
+        """
         Resets the file pointer or the rng generator to generate the same noise data
-        '''
+        """
         if self.read_from_file:
             self.fin_noise.seek(0, 0)
         else:
@@ -147,13 +135,11 @@ class NoiseIO:
 
     def generate_noise(self, batch_size):
         if self.read_from_file:
-            noise = np.fromfile(self.fin_noise, np.float32,
-                                batch_size * self.blk_len)
+            noise = np.fromfile(self.fin_noise, np.float32, batch_size * self.blk_len)
             noise = np.reshape(noise, [batch_size, self.blk_len])
         else:
             noise_awgn = self.rng.randn(batch_size, self.blk_len)
             noise_awgn = noise_awgn.astype(np.float32)
-            noise = self.sess.run(self.noise_tf, feed_dict={
-                                  self.awgn_noise: noise_awgn})
+            noise = self.sess.run(self.noise_tf, feed_dict={self.awgn_noise: noise_awgn})
 
         return noise
